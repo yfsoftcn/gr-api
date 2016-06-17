97d7801b62d18a35d1fed77255b85ee2edd428b4
yfdever <yfsoftcom@sina.com>
2016-06-17 12:25:22 +0800

    版本号更新

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ var yfserver = require('yf-api-server');
 
 var app = yfserver(config);
 
-app.setBizModules({'0.0.1':require('./V0.0.1')});//添加对应业务版本的代码
+app.setBizModules({'0.0.1':require('./V0.0.1')(config)});//添加对应业务版本的代码
 
 app.start();
