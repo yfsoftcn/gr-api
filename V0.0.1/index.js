0d35c0eded53d2fb41708453e007de6a888a94fe
yfdever <yfsoftcom@sina.com>
2016-06-18 08:48:30 +0800

    测试，开通一个common模块

--- V0.0.1/index.js
+++ V0.0.1/index.js
@@ -1,12 +1,12 @@
 'use strict';
 module.exports = function(C){
  return {
-        activity:require('./activity')(C),
-        api:require('./api')(C),
+        //activity:require('./activity')(C),
+        //api:require('./api')(C),
         common:require('./common')(C),
-        ec:require('./ec')(C),
-        erp:require('./erp')(C),
-        job:require('./job')(C),
-        order:require('./order')(C)
+        //ec:require('./ec')(C),
+        //erp:require('./erp')(C),
+        //job:require('./job')(C),
+        //order:require('./order')(C)
     };
 };
