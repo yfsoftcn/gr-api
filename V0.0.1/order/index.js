634569c81fece65c7101d0b6c5f6e2f076cc59f6
yfsoftcn <1794947912@qq.com>
2016-06-18 17:26:37 +0800

    Update index.js

--- V0.0.1/order/index.js
+++ V0.0.1/order/index.js
@@ -1,6 +1,6 @@
 'use strict';
 module.exports = function(M,B) {
-    var rest = M.rest;
+    var rest = B.rest;
     return function(args){
         var act = args.action;
         delete args.action;
